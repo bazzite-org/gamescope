052a9b2de8569c4e573b34b3d0d799679940bd92
Ruan E. Formigoni <ruanformigoni@gmail.com>
2025-01-02 17:07:36 +0100

    feat: implement bicubic downscaling
    From https://github.com/ValveSoftware/gamescope/pull/740

--- src/Backends/DRMBackend.cpp
+++ src/Backends/DRMBackend.cpp
@@ -3293,6 +3293,7 @@ namespace gamescope
 			bNeedsFullComposite |= bWasFirstFrame;
 			bNeedsFullComposite |= pFrameInfo->useFSRLayer0;
 			bNeedsFullComposite |= pFrameInfo->useNISLayer0;
+			bNeedsFullComposite |= pFrameInfo->useBICUBICLayer0;
 			bNeedsFullComposite |= pFrameInfo->blurLayer0;
 			bNeedsFullComposite |= bNeedsCompositeFromFilter;
 			bNeedsFullComposite |= !k_bUseCursorPlane && bDrewCursor;
